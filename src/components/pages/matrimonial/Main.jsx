81ea91141bcd524356eb7882fba44a620a4ce870
roshanbachhav <bachhavroshan600@gmail.com>
2025-08-12 18:37:24 +0530

    latest commit

--- src/components/pages/matrimonial/Main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const main = () => {
-  return <div>MatrimonialHome</div>;
-};
-
-export default Main;
---
+++ src/components/pages/matrimonial/MatrimonialMain.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import MatrimonialHome from "./MatrimonialHome";
+import Footer from "./Footer";
+import ProfileMatrimonial from "./ProfileMatrimonial";
+import MatrimonialHeader from "./matrimonialHeader";
+
+const MatrimonialMain = () => {
+  return (
+    <div>
+      {/* {/* <MatrimonialHeader /><MatrimonialHome />  */}
+      {/* <MatrimonialHeader /> */}
+      <MatrimonialHome />
+      {/* <Footer /> */}
+      {/* <ProfileMatrimonial /> */}
+    </div>
+  );
+};
+
+export default MatrimonialMain;
