27ff6f0670dd45fbdf81ce60636fb8dda7e28b02
RoshanAnantkamalstudios <roshan.anantkamalstudios@gmail.com>
2025-10-17 19:27:23 +0530

    console commit

--- src/components/pages/SubSection.jsx
+++ src/components/pages/SubSection.jsx
@@ -4,9 +4,6 @@ import { useParams, useLocation } from "react-router-dom";
 import ListView from "../layouts/Main/ListView";
 import GridView from "../layouts/Main/GridView";
 import MapView from "../layouts/Main/MapView";
-import { subVenuesData } from "../../data/subVenuesData";
-import { subVendorsData } from "../../data/subVendorsData";
-import { twoSoul } from "../../data/twoSoul";
 import MainSearch from "../layouts/Main/MainSearch";
 import PricingModal from "../layouts/PricingModal";
 import Photos from "../layouts/photography/Photos";
@@ -78,26 +75,14 @@ const SubSection = () => {
     setSelectedCity(reduxLocation);
   }, [cityFromQuery, reduxLocation]);
 
-  const getFallbackData = (section) => {
-    switch (section) {
-      case "vendors":
-        return subVendorsData;
-      case "venues":
-        return subVenuesData;
-      case "twosoul":
-        return twoSoul;
-      default:
-        return [];
-    }
-  };
-
   const dataToSend = useMemo(() => {
     if (section === "photography") {
       return [];
     }
 
+    // Return empty array if no data, don't use fallback dummy data
     if (error || !apiData || apiData.length === 0) {
-      return getFallbackData(section);
+      return [];
     }
 
     return apiData;
